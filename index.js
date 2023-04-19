37339820cc1e95c7a6ff86d3631c855bba3495ea
edie <dlthf@DESKTOP-VT5K4RD>
2023-04-19 19:07:19 +0900

    bodyparser추가 및 회원가입 통신소스시작

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const { User } = require("./models/User");
+const bodyPaser = require("body-parser");
+
+app.use(bodyPaser.urlencoded({ extended: true }));
+app.use(bodyPaser.json());
+
 const mongoose = require("mongoose");
 mongoose
   .connect(
@@ -16,5 +22,12 @@ mongoose
   .catch((err) => console.log("fail"));
 
 app.get("/", (req, res) => res.send("hello World"));
+
+app.post("/resiger", (req, res) => {
+  //회원가입할떄 필요한 정보들을 clinet에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User();
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
